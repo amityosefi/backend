69d507f2be06bcf9f28e677e4e6c771d040801fe
amit yosefi <amit.yosefi@gmail.com>
2021-07-23 15:26:29 +0300

    start

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app_utils = require("./app_utils");
 require("dotenv").config({path:'.env'})
-// declare a new express app
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/', async function(req, res) {
-  res.status(200).send("url to mp4 server is running");
+  res.status(200).send("server is running");
 });
 
-app.post('/converter', async function(req, res) {
+app.post('/images/:number', async function(req, res) {
   try {
-    const url = req.body.url;
-    if (!url) {
+    const number = req.body.number;
+    if (!number || isNaN(number)) {
       res.status(400).send({ message: 'Wrong inputs' });
     }
-    const ans = await app_utils.setFunc(url);
+    const ans = await app_utils.getImages(number);
     res.status(200).send(ans);
 
   } catch (err) {
@@ -27,7 +25,7 @@ app.post('/converter', async function(req, res) {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, function() {
     console.log(`App started listen on port ${port}`);
