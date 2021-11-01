b5a3208ded12e5fbc48f7b464cf5ff0566fbf677
amityosefi <amit.yosefi@gmail.com>
2021-11-01 16:37:11 +0200

    register, login. logout

--- project/routes/utils/auth_utils.js
+++ project/routes/utils/auth_utils.js
@@ -1,10 +1,30 @@
 const db_utils = require("./db_utils");
 
-async function getUserDetails(username, password){
-    const query = `SElECT * FROM dbo.users WHERE username = '${username}' and password = '${password}'`;
-    console.log(query);
+async function checkEmailExistence(Email){
+    const query = `SElECT count(*) as 'num' FROM dbo.users WHERE Email = '${Email}'`;
     const params = await db_utils.execQuery(query);
-    return params;
+    return params[0].num;
 }
 
-exports.getUserDetails = getUserDetails;
+async function checkUserPassword(Email){
+    const query = `SElECT Password FROM dbo.users WHERE Email = '${Email}'`;
+    const params = await db_utils.execQuery(query);
+    if(params[0])
+        return params[0].Password;
+    else
+        return undefined;
+    }
+async function insertNewUser(Email, Password, FirstName, LastName, Gender, Age){
+    const query = `INSERT INTO dbo.users (Email, Password, FirstName, LastName, Gender, Age) VALUES ('${Email}', '${Password}','${FirstName}', '${LastName}','${Gender}','${Age}')`;
+    const beforeInsert = await db_utils.execQuery(`SElECT count(*) as 'num' FROM dbo.users`);
+    const params = await db_utils.execQuery(query);
+    const afterInsert = await db_utils.execQuery(`SElECT count(*) as 'num' FROM dbo.users`);
+    if(afterInsert[0].num-beforeInsert[0].num > 0)
+        return {message: "User was added successfully"};
+    else
+        return {message: "There was a problem adding this user"};
+}
+
+exports.checkEmailExistence = checkEmailExistence;
+exports.checkUserPassword = checkUserPassword;
+exports.insertNewUser = insertNewUser;
